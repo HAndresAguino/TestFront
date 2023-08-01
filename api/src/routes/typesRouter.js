f239948f4182e27724fae9b4ad034aedcc71a7d3
Andres2498 <helan2498@gmail.com>
2023-08-01 10:14:06 -0500

    testfront1

--- api/src/routes/typesRouter.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require ('express')
-
-const {getTypesHandler} = require('../Handlers/typesHandler')
-
-const router = Router();
-
-router.get('/', getTypesHandler)
-
-module.exports = router
